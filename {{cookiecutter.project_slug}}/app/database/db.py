01ca8f8825d7a5cdb31fddd453e2d73730f067e8
nhjeon <nakhyun.jeon@gmail.com>
2020-06-29 15:38:58 +0900

    fix: SQLALCHEMY_DATABASE_URL

--- {{cookiecutter.project_slug}}/app/database/db.py
+++ {{cookiecutter.project_slug}}/app/database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-SQLALCHEMY_DATABASE_URL = {{ cookiecutter.mysql_url }}
+SQLALCHEMY_DATABASE_URL = "{{ cookiecutter.mysql_url }}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
