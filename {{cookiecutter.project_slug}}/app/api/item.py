99174d8328524bdab252552733ad293fcc8e8189
nhjeon <nakhyun.jeon@gmail.com>
2020-06-30 13:30:42 +0900

    feat: add response model in api

--- {{cookiecutter.project_slug}}/app/api/item.py
+++ {{cookiecutter.project_slug}}/app/api/item.py
@@ -1,10 +1,11 @@
 import logging
+from typing import List
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database.db import SessionLocal
-from model.item import ItemCreate, ItemUpdate
+from model.item import ItemCreate, ItemUpdate, Item
 from service import item_service
 
 router = APIRouter()
@@ -23,26 +24,26 @@ async def get_db():
         db.close()
 
 
-@router.get("/")
+@router.get("/", response_model=List[Item])
 def read_items(db: Session = Depends(get_db)):
     return item_service.read_items(db)
 
 
-@router.get("/{item_id}")
+@router.get("/{item_id}", response_model=Item)
 def read_item(item_id: int, db: Session = Depends(get_db)):
     return item_service.read_item(item_id, db)
 
 
-@router.post("/")
+@router.post("/", response_model=Item)
 def create_item(item: ItemCreate, db: Session = Depends(get_db)):
     return item_service.create_item(item, db)
 
 
-@router.put("/{item_id}")
+@router.put("/{item_id}", response_model=Item)
 def update_item(item_id: int, item: ItemUpdate, db: Session = Depends(get_db)):
     return item_service.update_item(item_id, item, db)
 
 
-@router.delete("/{item_id}")
+@router.delete("/{item_id}", response_model=Item)
 def delete_item(item_id: int, db: Session = Depends(get_db)):
     return item_service.delete_item(item_id, db)
