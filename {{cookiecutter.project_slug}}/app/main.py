e1a06a2f1741a245f8246c66128ae78fa919d90f
nhjeon <nakhyun.jeon@gmail.com>
2020-07-31 11:33:22 +0900

    feat: add logging format

--- {{cookiecutter.project_slug}}/app/main.py
+++ {{cookiecutter.project_slug}}/app/main.py
@@ -8,3 +8,6 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 app = FastAPI()
 app.include_router(api_router)
+
+FORMAT = '[%(asctime)s] [%(threadName)s(%(thread)d) [%(filename)s-%(lineno)d] [%(levelname)s] %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
