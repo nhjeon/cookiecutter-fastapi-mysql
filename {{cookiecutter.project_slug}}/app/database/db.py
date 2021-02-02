4bd1ba4c4da069f6eeaded74203b48794c11d493
nhjeon <nakhyun.jeon@gmail.com>
2021-02-02 17:08:02 +0900

    fix: change session to non-scoped session
    -Scoped session is not necessary
    Because session will be passed as a function parameter to the function
    If using scoped session, it could crash inside session.

--- {{cookiecutter.project_slug}}/app/database/db.py
+++ {{cookiecutter.project_slug}}/app/database/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "{{ cookiecutter.mysql_url }}"
 
@@ -8,6 +8,6 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     pool_pre_ping=True
 )
-SessionLocal = scoped_session(sessionmaker(autoflush=False, bind=engine))
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 Base = declarative_base()
